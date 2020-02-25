ff5000d60b0ecc80c2e46c54179478940df8fb76
Jan Kjærgaard <jan.k@nash.io>
2020-02-25 15:50:06 +0000

    fix native trampoline

--- src/native/index.ts
+++ src/native/index.ts
@@ -20,44 +20,41 @@ const loadNodeFile = (): NodeFileInterface => {
 
 const MpcWallet = loadNodeFile()
 console.log('using .node')
-export function dh_init(size: string, curve: string): Promise<string> {
+export function dh_init(size: string, curve: string): string {
   console.log(size, curve)
-  return Promise.resolve(MpcWallet.dh_init(parseInt(size, 10), JSON.parse(curve)))
+  return MpcWallet.dh_init(parseInt(size, 10), JSON.parse(curve))
 }
-export function fill_rpool(clientDHSecrets: string, serverDHPublics: string, curve: string): Promise<string> {
+export function fill_rpool(clientDHSecrets: string, serverDHPublics: string, curve: string): string {
   console.log(clientDHSecrets, serverDHPublics, curve)
-  return Promise.resolve(MpcWallet.fill_rpool(clientDHSecrets, serverDHPublics, JSON.parse(curve)))
+  return MpcWallet.fill_rpool(clientDHSecrets, serverDHPublics, JSON.parse(curve))
 }
-export function get_rpool_size(curve: string): Promise<string> {
-  console.log('get_rpool_size', curve)
-  const resp = MpcWallet.get_rpool_size(JSON.parse(curve))
-  console.log(resp)
-  return Promise.resolve(resp)
+export function get_rpool_size(curve: string): string {
+  return MpcWallet.get_rpool_size(JSON.parse(curve))
 }
-export function compute_presig(apiKeyStr: string, msgHashStr: string, curve: string): Promise<string> {
-  return Promise.resolve(MpcWallet.compute_presig(apiKeyStr, msgHashStr, JSON.parse(curve)))
+export function compute_presig(apiKeyStr: string, msgHashStr: string, curve: string): string {
+  return MpcWallet.compute_presig(apiKeyStr, msgHashStr, JSON.parse(curve))
 }
 
-export function init_api_childkey_creator(_: string): Promise<string> {
+export function init_api_childkey_creator(_: string): string {
   throw new Error('Not supported')
 }
-export function init_api_childkey_creator_with_verified_paillier(a: string, b: string): Promise<string> {
+export function init_api_childkey_creator_with_verified_paillier(a: string, b: string): string {
   console.log(a, b)
   throw new Error('Not supported')
 }
-export function verify_paillier(a: string, b: string, c: string): Promise<string> {
+export function verify_paillier(a: string, b: string, c: string): string {
   console.log(a, b, c)
   throw new Error('Not supported')
 }
-export function create_api_childkey(a: string, b: string): Promise<string> {
+export function create_api_childkey(a: string, b: string): string {
   console.log(a, b)
   throw new Error('Not supported')
 }
-export function verify(a: string, b: string, c: string, d: string, e: string): Promise<string> {
+export function verify(a: string, b: string, c: string, d: string, e: string): string {
   console.log(a, b, c, d, e)
   throw new Error('Not supported')
 }
-export function publickey_from_secretkey(a: string, b: string): Promise<string> {
+export function publickey_from_secretkey(a: string, b: string): string {
   console.log(a, b)
   throw new Error('Not supported')
 }
