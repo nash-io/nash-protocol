fb5420b007755e551c864b86cd6368972503d7df
tom <tom@nash.io>
2021-09-07 09:22:40 -0500

    Update out of date packages

--- src/mpc/computePresig.ts
+++ src/mpc/computePresig.ts
@@ -1,8 +1,17 @@
 import { fillRPoolIfNeeded } from './fillRPool'
 import { ComputePresigParams, BlockchainCurve, Presignature } from '../types/MPC'
 
+let MPCWallet
+
+const getMPCWallet = async (): Promise<any> => {
+  if (MPCWallet == null) {
+    MPCWallet = await import('../mpc-lib')
+  }
+  return MPCWallet
+}
+
 export async function computePresig(params: ComputePresigParams): Promise<Presignature> {
-  const MPCWallet = await import('../mpc-lib')
+  const wallet = await getMPCWallet()
   await fillRPoolIfNeeded({
     blockchain: params.blockchain,
     fillPoolFn: params.fillPoolFn,
@@ -10,7 +19,7 @@ export async function computePresig(params: ComputePresigParams): Promise<Presig
   })
 
   const [comutePresigOk, presigOrErrorMessage, r] = JSON.parse(
-    MPCWallet.compute_presig(JSON.stringify(params.apiKey), params.messageHash, BlockchainCurve[params.blockchain])
+    wallet.compute_presig(JSON.stringify(params.apiKey), params.messageHash, BlockchainCurve[params.blockchain])
   ) as [boolean, string, string]
   if (comutePresigOk === false) {
     throw new Error('Error computing presig: ' + presigOrErrorMessage)
