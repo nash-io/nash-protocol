0a711cee55dd967724c328b3fe3dc8d62691596f
tom <tom@nash.io>
2025-02-28 11:32:34 -0600

    updated wasm build and node version

--- src/mpc/publicKeyFromSecretKey.ts
+++ src/mpc/publicKeyFromSecretKey.ts
@@ -2,9 +2,10 @@ import { PublicKeyFromSecretKeyParams } from '../types/MPC'
 
 export async function publicKeyFromSecretKey({ secret, curve }: PublicKeyFromSecretKeyParams): Promise<string> {
   const MPCWallet = await import('../mpc-lib')
-  const [publicKeyFromSecretKeySuccess, publicKeyResult] = JSON.parse(
-    MPCWallet.publickey_from_secretkey(secret, curve)
-  ) as [boolean, string]
+  const [publicKeyFromSecretKeySuccess, publicKeyResult] =
+    JSON.parse(
+      MPCWallet.publickey_from_secretkey(secret, JSON.stringify(curve))
+    ) as [boolean, string]
   if (publicKeyFromSecretKeySuccess === false) {
     throw new Error('Error deriving public key from secret key.')
   }
