3ab975df5ab7cca692de900b03f75e4fc0faf300
tom <tom@nash.io>
2025-03-13 10:51:27 -0500

    4.99.8 is a debug build with export of of wasm as 'bundler'

--- src/wasm/mpc_wallet_wasm_bg.wasm.d.ts
+++ src/wasm/mpc_wallet_wasm_bg.wasm.d.ts
@@ -12,10 +12,10 @@ export const compute_presig: (a: number, b: number, c: number, d: number, e: num
 export const verify: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number) => [number, number];
 export const publickey_from_secretkey: (a: number, b: number, c: number, d: number) => [number, number];
 export const sign: (a: number, b: number, c: number, d: number) => [number, number];
-export const rustsecp256k1_v0_2_0_default_illegal_callback_fn: (a: number, b: number) => void;
-export const rustsecp256k1_v0_2_0_default_error_callback_fn: (a: number, b: number) => void;
 export const rustsecp256k1_v0_2_0_context_create: (a: number) => number;
 export const rustsecp256k1_v0_2_0_context_destroy: (a: number) => void;
+export const rustsecp256k1_v0_2_0_default_illegal_callback_fn: (a: number, b: number) => void;
+export const rustsecp256k1_v0_2_0_default_error_callback_fn: (a: number, b: number) => void;
 export const __wbindgen_export_0: WebAssembly.Table;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
